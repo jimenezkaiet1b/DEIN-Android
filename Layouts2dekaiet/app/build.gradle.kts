152f19ec9a132ac0dda1ff18cd152b6d1e7d9de6
Kaiet <kjimeneald@educacion.navarra.es>
2024-01-16 17:31:45 +0100

    Primera parte acabada

--- Layouts2dekaiet/app/build.gradle.kts
+++ Layouts2dekaiet/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.layouts2dekaiet"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.layouts2dekaiet"
