721dd76cbe606c9cf57a2665854eabe319e1b895
kaiet <kjimeneald@educacion.navarra.es>
2024-01-16 08:39:44 +0100

    Acabado

--- Layouts1Kaiet/app/build.gradle.kts
+++ Layouts1Kaiet/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.layout1dekaiet"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.layout1dekaiet"
--- Layouts1Kaiet/app/src/main/java/com/example/layout1dekaiet/MainActivity.kt
+++ Layouts1Kaiet/app/src/main/java/com/example/layout1dekaiet/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.layout1dekaiet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.frame_layout)
+
+        val botonSuperior : Button = findViewById(R.id.superior)
+        val botonCentro : Button = findViewById(R.id.cenro)
+        val botonInferior : Button = findViewById(R.id.inferior)
+
+        botonSuperior.setOnClickListener{
+            setContentView(R.layout.linear_layout)
+        }
+        botonCentro.setOnClickListener{
+            setContentView(R.layout.linear_layout)
+        }
+        botonInferior.setOnClickListener{
+            setContentView(R.layout.linear_layout)
+        }
+
+
+
+
+
+
+
+    }
+    fun click (){
+
+
+
     }
 }
