c387ef0875ed49c7f45d7367384da0aa380d94d4
Kaiet <kjimeneald@educacion.navarra.es>
2024-01-17 19:53:19 +0100

    Proyectocreado

---
+++ Layout3deKaiet/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Layouts 3 de Kaiet"
+include(":app")
+ 
