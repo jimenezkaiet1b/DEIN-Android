65cc89cb7414d5064003baac4783e41764d510aa
Kaiet <kjimeneald@educacion.navarra.es>
2024-02-01 19:02:42 +0100

    menu3

--- MenuPopupMenu/app/src/main/java/com/example/menupopupmenu/MainActivity.kt
+++ MenuPopupMenu/app/src/main/java/com/example/menupopupmenu/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.menupopupmenu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.PopupMenu
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val moreButton: ImageView = findViewById(R.id. more_actions_button)
+        moreButton.setOnClickListener { view ->
+            showMoreActionsMenu(view)
+        }
+    }
+    private fun showMoreActionsMenu(button: View) {
+        val popupMenu = PopupMenu(this, button)
+        menuInflater.inflate(R.menu.popup_menu, popupMenu.menu)
+        popupMenu.show()
     }
 }
