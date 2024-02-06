fd1c76bc177627d63275a75ddafd5e3e3d24c6d8
Kaiet <kjimeneald@educacion.navarra.es>
2024-02-06 17:42:18 +0100

    Acabado

---
+++ MenudeNavegacion/app/src/main/java/com/example/menudenavegacion/PageFragment.kt
@@ -0,0 +1,28 @@
+package com.example.menudenavegacion
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.DrawableRes
+import androidx.fragment.app.Fragment
+
+class PageFragment : Fragment() {
+    override fun onCreateView (inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?):
+            View? {
+        val layout = inflater.inflate(R.layout. fragment_page, container, false)
+        val icon = arguments?.getInt(ARG_ICON, 0) ?: 0
+        layout.findViewById<View>(R.id. imageView).setBackgroundResource(icon)
+        return layout
+    }
+    companion object {
+        private const val ARG_ICON = "ARG_ICON"
+        fun newInstance (@DrawableRes iconId: Int): PageFragment {
+            val fragment = PageFragment()
+            val args = Bundle()
+            args.putInt( ARG_ICON, iconId)
+            fragment. arguments = args
+            return fragment
+        }
+    }
+}
